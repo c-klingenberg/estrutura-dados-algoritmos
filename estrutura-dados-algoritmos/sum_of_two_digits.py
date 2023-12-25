c50ed45fa599e55183cd7d07b6a1b1421f87edfc
Carolina Klingenberg <carolinaklingenberg@live.de>
2023-12-25 12:36:36 -0300

    Últimas atualizações

---
+++ estrutura-dados-algoritmos/sum_of_two_digits.py
@@ -0,0 +1,7 @@
+def sum_two_digits(first_digit, second_digit):
+    return int(first_digit) + int(second_digit)
+
+a = input("Enter the first digit: ")
+b = input("Enter the second digit: ")
+
+print(sum_two_digits(a, b))
